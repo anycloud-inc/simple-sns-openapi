d3ddbc0d3d96f0e579483b776d799a5ed1801252
KazutakaShimizu <s.kazutaka55555@gmail.com>
2022-01-24 17:54:32 +0900

    openapi.yamlからtsの型ファイルを生成するように

--- generate.sh
+++ generate.sh
@@ -1,1 +1,13 @@
-npx redoc-cli bundle openapi.yaml -o document.html
+#!/bin/sh
+set -ex
+
+# ドキュメントを生成
+npx redoc-cli bundle ./specs/openapi.yaml -o document.html
+
+# 分割ファイルを統合
+npx swagger-cli bundle ./specs/openapi.yaml -o ./build/openapi.yaml -t yaml
+
+# tsの型ファイルを生成
+rm -r outputs/openapi_server_interface/ts/types.ts
+npx openapi-typescript ./build/openapi.yaml \
+  --output outputs/openapi_server_interface/ts/types.ts
